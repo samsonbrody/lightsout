45928a0b6a6a3a98b569e506bcdcc04659d5c693
samsonbrody <samsonbrody@gmail.com>
2021-09-29 16:57:02 -0700

    Updates

---
+++ precache-manifest.4c37a55dbf763ba055aee53259eb1b94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3a82a0d193ffe816638",
+    "url": "/lightsout/static/css/main.2afed5a8.chunk.css"
+  },
+  {
+    "revision": "e3a82a0d193ffe816638",
+    "url": "/lightsout/static/js/main.e3a82a0d.chunk.js"
+  },
+  {
+    "revision": "a989bfac1d920b4f2190",
+    "url": "/lightsout/static/js/1.a989bfac.chunk.js"
+  },
+  {
+    "revision": "5140b50a75f4729ab6d5",
+    "url": "/lightsout/static/js/runtime~main.5140b50a.js"
+  },
+  {
+    "revision": "6cb4265724d1ffb9538ecba9312e2f08",
+    "url": "/lightsout/index.html"
+  }
+];
